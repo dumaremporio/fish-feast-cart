817591d6d9ea7f3f6b603f6b46cadd0e439dd7c7
dumaremporio <dumaremporiosp@gmail.com>
2026-02-22 10:40:21 -0300

    Rename import paths for pages directory

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import Index from "./páginas/Index";
-import B2BRegister from "./páginas/B2BRegister";
-import NotFound from "./páginas/NotFound";
+import Index from "./pages/Index";
+import B2BRegister from "./pages/B2BRegister";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
