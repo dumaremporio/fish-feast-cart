d3427b2909573d6500e489fbce8a62b906a722f4
dumaremporio <dumaremporiosp@gmail.com>
2026-02-22 09:10:04 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/fish-feast-cart/",
   server: {
     host: "::",
     port: 8080,
