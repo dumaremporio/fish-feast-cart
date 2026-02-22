a81e31468d698afe4e4bb0dee67654dff17c5972
dumaremporio <dumaremporiosp@gmail.com>
2026-02-22 10:58:49 -0300

    Refactor App component to remove unused providers
    Removed unused providers and simplified the App component structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"
+import Index from "./pages/Index"
+import B2BRegister from "./pages/B2BRegister"
+import NotFound from "./pages/NotFound"
 
-import Index from "./pages/Index";
-import B2BRegister from "./pages/B2BRegister";
-import NotFound from "./pages/NotFound";
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/b2b" element={<B2BRegister />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  )
+}
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/b2b" element={<B2BRegister />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default App
